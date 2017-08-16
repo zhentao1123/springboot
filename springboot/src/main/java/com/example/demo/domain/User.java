373dbef5c135437d4f82ca9e4c2924a31163f451
zhentao1123 <zzt1981@126.com>
2017-08-16 18:04:12 +0800

    update annotations param value of swagger

--- springboot/src/main/java/com/example/demo/domain/User.java
+++ springboot/src/main/java/com/example/demo/domain/User.java
@@ -1,10 +1,18 @@
 package com.example.demo.domain;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Data
+@ApiModel(description="用户模型")
 public class User {
+	@ApiModelProperty(name="ID")
 	private Long id;
+	
+	@ApiModelProperty(name="姓名")
     private String name;
+	
+	@ApiModelProperty(name="年龄")
     private Integer age;
 }
--- springboot/src/main/java/com/example/demo/web/UserController.java
+++ springboot/src/main/java/com/example/demo/web/UserController.java
@@ -1,5 +1,6 @@
 package com.example.demo.web;
 
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
@@ -10,8 +11,11 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,9 +29,12 @@ import com.example.demo.domain.User;
  */
 @RestController
 @RequestMapping("/users")
+@Api(hidden=false)
 public class UserController {
 	
 	public static final String SUCCESS = "success";
+	
+	static final Log log = LogFactory.getLog(UserController.class);
 	static Map<Long, User> users = Collections.synchronizedMap(new HashMap<Long, User>());
 
 	/**
@@ -51,8 +58,9 @@ public class UserController {
 	@ApiOperation(value="创建用户", notes="根据User对象创建用户")
     @ApiImplicitParam(name = "user", value = "用户详细实体user", required = true, dataType = "User")
 	@RequestMapping(value="/", method=RequestMethod.POST)
-	public String postUser(@ModelAttribute User user){
+	public String postUser(@RequestBody User user){
 		users.put(user.getId(), user);
+		log.warn(users.toString());
 		return SUCCESS;
 	}
 	
@@ -63,7 +71,7 @@ public class UserController {
 	 * @return
 	 */
 	@ApiOperation(value="获取用户详细信息", notes="根据url的id来获取用户详细信息")
-    @ApiImplicitParam(name = "id", value = "用户ID", required = true, dataType = "Long")
+    @ApiImplicitParam(name = "id", value = "用户ID", required = true, dataType = "Long", paramType = "path")
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
 	public User getUser(@PathVariable Long id){
 		return users.get(id);
@@ -77,11 +85,11 @@ public class UserController {
 	 */
 	@ApiOperation(value="更新用户详细信息", notes="根据url的id来指定更新对象，并根据传过来的user信息来更新用户详细信息")
     @ApiImplicitParams({
-            @ApiImplicitParam(name = "id", value = "用户ID", required = true, dataType = "Long"),
+            @ApiImplicitParam(name = "id", value = "用户ID", required = true, dataType = "Long", paramType = "path"),
             @ApiImplicitParam(name = "user", value = "用户详细实体user", required = true, dataType = "User")
     })
 	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
-	public String putUser(@PathVariable Long id, @ModelAttribute User user){
+	public String putUser(@PathVariable Long id, @RequestBody User user){
 		User u = users.get(id);
 		u.setAge(user.getAge());
 		u.setName(user.getName());
@@ -95,7 +103,7 @@ public class UserController {
 	 * @return
 	 */
 	@ApiOperation(value="删除用户", notes="根据url的id来指定删除对象")
-    @ApiImplicitParam(name = "id", value = "用户ID", required = true, dataType = "Long")
+    @ApiImplicitParam(name = "id", value = "用户ID", required = true, dataType = "Long", paramType = "path")
 	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
 	public String deleteUser(@PathVariable Long id) {
 		users.remove(id);
