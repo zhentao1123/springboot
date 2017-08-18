c5422c162bdfbe59cfd1294d9e97aa3d09504675
zhentao1123 <zzt1981@126.com>
2017-08-18 09:55:01 +0800

    modify class of config

--- springboot/src/main/java/com/example/demo/Application.java
+++ springboot/src/main/java/com/example/demo/Application.java
@@ -2,10 +2,16 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.context.annotation.Import;
+
+import com.example.demo.config.DataSourceConfig;
+import com.example.demo.config.Swagger2Config;
+
+import springfox.documentation.swagger2.configuration.Swagger2DocumentationConfiguration;
 
 @SpringBootApplication
-//@EnableWebMvc
+//@EnableWebMvc//开启springmvc
+//@Import({Swagger2Config.class, DataSourceConfig.class})
 public class Application {
 
 	public static void main(String[] args) {
--- springboot/src/main/java/com/example/demo/config/Swagger2Config.java
+++ springboot/src/main/java/com/example/demo/config/Swagger2Config.java
@@ -1,15 +1,13 @@
-package com.example.demo;
+package com.example.demo.config;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.context.request.async.DeferredResult;
 
-import com.google.common.base.Predicates;
-
 import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
@@ -19,21 +17,22 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-public class Swagger2 {
+public class Swagger2Config {
 	
 	static final String basePackage = "com.example.demo.web";
 	
+	@Bean
 	public Docket createRestApi(){
 		return new Docket(DocumentationType.SWAGGER_2)
-			.groupName("v1")
-			.genericModelSubstitutes(DeferredResult.class)
-            .useDefaultResponseMessages(false)
-            .forCodeGeneration(true)
-            .pathMapping("/")
+			//.groupName("MyGroup")
+			//.genericModelSubstitutes(DeferredResult.class)
+            //.useDefaultResponseMessages(false)
+            //.forCodeGeneration(true)
+            //.pathMapping("/")
 			.select()
 			//.apis(RequestHandlerSelectors.basePackage(basePackage))//匹配包
 			//.apis(RequestHandlerSelectors.withClassAnnotation(Api.class))//匹配类注解
-			//.apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))//匹配方法注解
+			.apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))//匹配方法注解
 			//.paths(PathSelectors.any())
 			//.paths(Predicates.or(PathSelectors.regex("/users/.*")))//过滤的接口
 			.build()
