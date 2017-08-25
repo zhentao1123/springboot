25100c2436be581e33a6f9ad6991e92e764911a5
zhentao1123 <zzt1981@126.com>
2017-08-25 10:24:00 +0800

    update

--- springboot/src/main/java/com/example/demo/web/filter/DruidStatFilter.java
+++ springboot/src/main/java/com/example/demo/web/filter/DruidStatFilter.java
@@ -9,7 +9,7 @@ import com.alibaba.druid.support.http.WebStatFilter;
 initParams={
     @WebInitParam(name="exclusions",value="*.js,*.gif,*.jpg,*.bmp,*.png,*.css,*.ico,/druid/*"),// 忽略资源
     @WebInitParam(name="sessionStatMaxCount",value="1000"),//缺省1000
-    //@WebInitParam(name="sessionStatEnable",value="false"),//可以关闭season监控
+    @WebInitParam(name="sessionStatEnable",value="true"),//可以关闭season监控
     @WebInitParam(name="profileEnable",value="true"),//druid 0.2.7版本开始支持profile，配置profileEnable能够监控单个url调用的sql列表
 })
 public class DruidStatFilter extends WebStatFilter{
