a5a0f59a281f0f44151dfc8867c78b68b4b63849
zhentao1123 <zzt1981@126.com>
2017-09-01 18:06:48 +0800

    update

--- springboot/src/main/java/com/example/demo/config/JsonConfig.java
+++ springboot/src/main/java/com/example/demo/config/JsonConfig.java
@@ -3,11 +3,13 @@ package com.example.demo.config;
 import org.springframework.boot.autoconfigure.web.HttpMessageConverters;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 
 import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.alibaba.fastjson.support.config.FastJsonConfig;
 import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+import com.google.common.collect.Lists;
 
 @Configuration
 public class JsonConfig {
@@ -15,9 +17,24 @@ public class JsonConfig {
 	@Bean
     public HttpMessageConverters fastJsonHttpMessageConverters() {
        FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter();
+       
        FastJsonConfig fastJsonConfig = new FastJsonConfig();
-       fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat);
+       fastJsonConfig.setSerializerFeatures(
+    		   SerializerFeature.PrettyFormat, 
+    		   SerializerFeature.QuoteFieldNames, 
+    		   SerializerFeature.WriteDateUseDateFormat,
+    		   SerializerFeature.WriteNullListAsEmpty,
+    		   SerializerFeature.DisableCircularReferenceDetect);
+       fastJsonConfig.setDateFormat("yyyy-MM-dd HH:mm:ss");
+       
+       //fastJsonConfig.setParserConfig(parserConfig);
        fastConverter.setFastJsonConfig(fastJsonConfig);
+       
+       fastConverter.setSupportedMediaTypes(MediaType.parseMediaTypes(Lists.newArrayList(
+    		   MediaType.APPLICATION_JSON_UTF8_VALUE, 
+    		   MediaType.TEXT_HTML_VALUE+";charset=UTF-8"
+    		   )));
+       
        HttpMessageConverter<?> converter = fastConverter;
        return new HttpMessageConverters(converter);
     }
