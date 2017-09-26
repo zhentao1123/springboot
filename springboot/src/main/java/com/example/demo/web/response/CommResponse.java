dda0c67289dbbfd09747558d785ba65482515a09
zhentao1123 <zzt1981@126.com>
2017-09-26 08:57:14 +0800

    add get set

--- springboot/src/main/java/com/example/demo/web/response/CommResponse.java
+++ springboot/src/main/java/com/example/demo/web/response/CommResponse.java
@@ -7,7 +7,6 @@ import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Data
-<<<<<<< HEAD
 @ApiModel(description="响应模型")
 public class CommResponse<D> {
 	
@@ -68,43 +67,4 @@ public class CommResponse<D> {
 		this.data = data;
 	}
 
-=======
-//@ApiModel(description="响应模型")
-public class CommResponse<D> {
-	
-	public static final int CODE_SUCCEED = 1;
-	public static final int CODE_FAIL = 0;
-	public static final String COMM_MESSAGE_SUCCEED = "succeed";
-	public static final String COMM_MESSAGE_FAIL = "fail";
-	
-	//@ApiModelProperty(name="code")
-	private Integer code;
-	//@ApiModelProperty(name="message")
-	private String message;
-	
-	/**
-	 * virtual response data
-	 */
-	//@ApiModelProperty(name="data")
-	private D data;
-	
-	public CommResponse(Integer code, String message, D data) {
-		super();
-		this.code = code;
-		this.message = message;
-		this.data = data;
-	}
-	
-	public static <D> CommResponse<D> getInstances4Succeed(D data){
-		CommResponse<D> instances = new CommResponse<D>(CODE_SUCCEED, COMM_MESSAGE_SUCCEED, data);
-		return instances;
-	}
-
-	public static <D> CommResponse<D> getInstances4Fail(D data){
-		CommResponse<D> instances = new CommResponse<D>(CODE_FAIL, COMM_MESSAGE_FAIL, null);
-		return instances;
-	}
-
-	
->>>>>>> branch 'master' of https://github.com/zhentao1123/springboot.git
 }
